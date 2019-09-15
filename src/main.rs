37abb3a9406f25d8dff87466c30a53f98f4891da
rodrigovalle <rdan.valle@gmail.com>
2019-09-15 20:30:24 +0000

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,6 @@ use grammar::SExpr;
 use lex::Parse;
 
 fn main() {
-    //let tokenizer = Lexer::new("(define hello (3 4))");
-    //for token in tokenizer {
-    //    print!("{:?} ", token);
-    //}
     match SExpr::parse("(+ (- 1 2) () 3 4)") {
         Ok((sexpr, _)) => {
             println!("success");
